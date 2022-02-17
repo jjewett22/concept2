0a5bd4a2ce8c4a0b3f7ca699bb35dc68a3e5f2a5
jjewett22 <jjewett22@gmail.com>
2022-02-17 01:40:02 -0500

    Add files via upload

---
+++ ContrastformController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContrastformController extends Controller
+{
+    
+    
+     
+    public function index(Request $request)
+    {
+        
+        
+        
+        
+        $name = $request->input('name');
+        $bun = $request->input('bun');
+        $creatinine = $request->input('creatinine');
+        $gfr = $request->input('gfr');
+        $contrasttype = $request->input('contrasttype');
+        $contrastamount = $request->input('contrastamount');
+        $needlegauge = $request->input('needlegauge');
+        $contrastreaction = $request->input('contrastreaction');
+          
+        
+       
+        return view('/contrastform' , compact('name','bun' ,'creatinine', 'gfr', 'contrasttype', 'needlegauge' ,'contrastamount', 'contrastreaction'));
+ 
+        
+    }
+    //dd($request->all);
+
+}
